e38c00629d62928ef14e59926491e345576d03cb
Michael Waskom <mwaskom@stanford.edu>
2012-07-03 13:32:30 -0700

    Adding sigstars

--- moss/misc.py
+++ moss/misc.py
@@ -3,5 +3,22 @@ from IPython.core.display import Javascript, display
 
 
 def save_notebook():
-    """Save an IPython notebook from running code."""
+    """Save an IPython notebook from running code.
+
+    Note this must be returned from a notebook cell.
+
+    """
     display(Javascript('IPython.notebook.save_notebook()'))
+
+
+def sig_stars(p):
+    """Return a R-style significance string corresponding to p values."""
+    if p < 0.001:
+        return "***"
+    elif p < 0.01:
+        return "**"
+    elif p < 0.05:
+        return "*"
+    elif p < 0.1:
+        return "."
+    return ""
