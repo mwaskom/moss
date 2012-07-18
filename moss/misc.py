087b088c0c69f9c84eae063cd747851ec7eef481
Michael Waskom <mwaskom@stanford.edu>
2012-07-18 13:33:23 -0700

    Schedule helper function

--- moss/misc.py
+++ moss/misc.py
@@ -1,5 +1,39 @@
 """Miscellaneous utility functions."""
+import numpy as np
 from IPython.core.display import Javascript, display
+
+
+def make_master_schedule(evs):
+    """Take a list of event specifications and make one schedule.
+
+    Parameters
+    ----------
+    evs : sequence of n x 3 arrays
+        list of (onset, duration, amplitude) event secifications
+
+    Returns
+    -------
+    sched : n_event x 5 array
+        schedule of event specifications with
+        event and presentation ids
+
+    """
+    evs = np.asarray(evs)
+    n_cond = len(evs)
+
+    # Make a vector of condition ids and stimulus indices
+    cond_ids = [np.ones(evs[i].shape[0]) * i for i in range(n_cond)]
+    cond_ids = np.concatenate(cond_ids)
+    stim_idxs = np.concatenate([np.arange(len(ev)) for ev in evs])
+
+    # Make a schedule of the whole run
+    sched = np.row_stack(evs)
+    sched = np.column_stack((sched, cond_ids, stim_idxs))
+
+    # Sort the master schedule by onset time
+    timesorter = np.argsort(sched[:, 0])
+    sched = sched[timesorter]
+    return sched
 
 
 def save_notebook():
