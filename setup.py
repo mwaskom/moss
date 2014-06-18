b123cea3b5e0801b7623624d212b8aece808daef
Michael Waskom <mwaskom@stanford.edu>
2014-06-18 15:01:57 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ MAINTAINER_EMAIL = 'mwaskom@stanford.edu'
 LICENSE = 'BSD (3-clause)'
 URL = 'https://github.com/mwaskom/moss'
 DOWNLOAD_URL = 'https://github.com/mwaskom/moss'
-VERSION = '0.3.dev'
+VERSION = '0.3'
 
 from setuptools import setup
 
