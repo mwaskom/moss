47d381f5a40998018cdaed5df0e485352e5a5bd7
Michael Waskom <mwaskom@stanford.edu>
2014-06-18 15:13:57 -0700

    Format version with three points

--- moss/__init__.py
+++ moss/__init__.py
@@ -3,4 +3,4 @@ from .misc import *
 from . import design
 from . import glm
 
-__version__ = "0.3"
+__version__ = "0.3.0"
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ MAINTAINER_EMAIL = 'mwaskom@stanford.edu'
 LICENSE = 'BSD (3-clause)'
 URL = 'https://github.com/mwaskom/moss'
 DOWNLOAD_URL = 'https://github.com/mwaskom/moss'
-VERSION = '0.3'
+VERSION = '0.3.0'
 
 from setuptools import setup
 
